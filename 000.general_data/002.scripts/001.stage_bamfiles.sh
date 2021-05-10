e33282a26982ad6e01134725783f7ed6a281d93d
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-05-10 13:48:02 +0200

    stage bamfiles to workdir

---
+++ 000.general_data/002.scripts/001.stage_bamfiles.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+# stage rawdata bamfiles from rawdata repository to scratch-cbe
+
+# SLURM #
+#SBATCH --output 999.logs/stage_bamfiles.log
+#SBATCH --time=02:00:00
+#SBATCH --mem=20GB
+
+SAMPLES=/groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/000.general_data/001.data/samples.txt
+BAMFILES=($(awk 'NR>1 {print $10}' $SAMPLES))
+
+TARGET=/scratch-cbe/users/pieter.clauw/003.transcriptome/001.8accessions/000.general_data/001.data/001.bamfiles/
+mkdir -p $TARGET
+
+for BAM in ${BAMFILES[@]}; do
+    cp -v $BAM $TARGET
+done
