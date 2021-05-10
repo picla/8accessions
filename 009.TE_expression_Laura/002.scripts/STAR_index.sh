2fa6aa187296f98b8122b45d5328988ef25e77f2
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-05-10 11:47:11 +0200

    build index for TE consensus

---
+++ 009.TE_expression_Laura/002.scripts/STAR_index.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# SLURM #
+#SBATCH --output 999.logs/STAR_index.log
+#SBATCH --mem=3GB
+#SBATCH --time=00:10:00
+#SBATCH --cpus-per-task=4
+
+# MODULES #
+ml star/2.7.1a-foss-2018b
+
+# map RNA-seq reads to consensus TE sequences
+
+
+# DATA #
+mainDir=/scratch-cbe/users/pieter.clauw/003.transcriptome/001.8accessions/009.TE_expression_Laura
+
+# stage in
+cp -v /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/009.TE_expression_Laura/001.data/Ath_RepBase_TEs.fasta ${mainDir}/001.data
+
+FASTA=${mainDir}/001.data/Ath_RepBase_TEs.fasta
+Indices=${FASTA/.fasta/_STAR_idx/}
+
+# PARAMETERS #
+cores=4
+
+# PREPARATION #
+mkdir -p $Indices
+
+# GENOME INDICES #
+
+STAR \
+--runThreadN $cores \
+--runMode genomeGenerate \
+--genomeDir $Indices \
+--genomeFastaFiles $FASTA
+
+# stage out
+cp -rv $Indices /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/009.TE_expression_Laura/001.data/
+
+
+
