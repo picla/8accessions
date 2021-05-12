8e4b60f4a407a7c557e6ddbce0c4331365b9b452
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-05-12 14:45:21 +0200

    stage trimmed fastq files to scratch

---
+++ 009.TE_expression_Laura/002.scripts/001.stage_in.sh
@@ -0,0 +1,2 @@
+
+stage /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/000.general_data/001.data/001.fastq_trimmed/ /scratch-cbe/users/pieter.clauw/003.transcriptome/001.8accessions/000.general_data/001.data/002.fastqfiles/
