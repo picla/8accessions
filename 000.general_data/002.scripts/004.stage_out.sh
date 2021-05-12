ff1a146cf47fe7e90a42263099680f2114104758
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-05-12 14:44:54 +0200

    stage trimmed fastq files to projects

---
+++ 000.general_data/002.scripts/004.stage_out.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# stage out trimmed fastq files
+# trimmed fastq files are generated with 003.trimming.sh
+
+stage /scratch-cbe/users/pieter.clauw/003.transcriptome/001.8accessions/000.general_data/001.data/002.fastqfiles/ /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/000.general_data/001.data/001.fastq_trimmed/
