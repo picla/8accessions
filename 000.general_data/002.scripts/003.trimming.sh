25df3a2ce2ff03462e1c13a0fb2b509e73143e97
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-05-14 15:28:26 +0200

    trimming with multithread

--- 000.general_data/002.scripts/003.trimming.sh
+++ 000.general_data/002.scripts/003.trimming.sh
@@ -4,22 +4,26 @@
 #SBATCH --output 999.logs/trim_galore_%A_%a.log
 #SBATCH --time=01:00:00
 #SBATCH --mem=1GB
-#SBATCH --array=1-95:2
+#SBATCH --array=0-94:2
+#SBATCH --cpus-per-task=15
 
 # MODULES #
 ml trim_galore/0.6.2-foss-2018b-python-3.6.6
 
 # DATA #
-end1=$SLURM_ARRAY_TASK_ID
-end2=$(expr $end1 + 1)
+i=$SLURM_ARRAY_TASK_ID
 DATAdir=/scratch-cbe/users/pieter.clauw/003.transcriptome/001.8accessions/000.general_data/001.data
+FASTQfiles=(${DATAdir}/002.fastqfiles/*.fastq)
 OUTdir=${DATAdir}/003.fastq_trimmed/
-FASTQlst=${DATAdir}/002.fastqfiles/FASTQ_list.txt
+
+FASTQ1=${FASTQfiles[$i]}
+FASTQ2=${FASTQ1/end1/end2}
+
 # ls ${DATAdir}/002.fastqfiles/*.fastq > $FASTQlst
 
 # take fastqs from fastqList.txt
-FASTQ1=$(sed "${end1}q;d" $FASTQlst)
-FASTQ2=$(sed "${end2}q;d" $FASTQlst)
+#FASTQ1=$(sed "${end1}q;d" $FASTQlst)
+#FASTQ2=$(sed "${end2}q;d" $FASTQlst)
 
 # start trim_galor
 echo 'starting trim_galore on files:'
@@ -32,5 +36,6 @@ trim_galore -q 10 \
 	--phred33 \
 	--paired \
 	--nextera \
+	--cores 4 \
 	$FASTQ1 $FASTQ2
 
