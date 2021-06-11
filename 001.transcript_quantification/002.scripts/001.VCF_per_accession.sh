c3110a9fdadc2815801dcb3503de3ef2a256222d
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-11 17:04:45 +0200

    VCF per accession

---
+++ 001.transcript_quantification/002.scripts/001.VCF_per_accession.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# SLURM
+#SBATCH --mem=5GB
+#SBATCH --time=03:00:00
+#SBATCH --output=999.logs/subsetVCF_%A_%a.log
+#SBATCH --array=0-6
+
+ml vcftools/0.1.16-foss-2018b-perl-5.28.0
+
+# DATA #
+i=$SLURM_ARRAY_TASK_ID
+WORK=/scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/
+VCF=${WORK}001.data/1001genomes_snp-short-indel_only_ACGTN.vcf.gz
+
+# copy VCF from source if most recent not yet present in destination
+cp -up /groups/nordborg/projects/the1001genomes/release/VCFs/1001genomes_snp-short-indel_only_ACGTN.vcf.gz
+
+accessions=(6017 9728 9559 8242 9888 9433 9075)
+
+acn=${accessions[$i]}
+
+out=${VCF/.vcf.gz/_${acn}.vcf}
+vcf-subset -c $acn $VCF > $out
+
+
