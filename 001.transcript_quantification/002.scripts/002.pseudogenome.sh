d60c659b6e5fbe7d4722d51a14960ee612e49bd8
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-11 17:01:30 +0200

    make pseuogenomes

---
+++ 001.transcript_quantification/002.scripts/002.pseudogenome.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# SLURM
+#SBATCH --output=999.logs/pseudotranscriptome_%A_%a.log
+#SBATCH --time=12:00:00
+#SBATCH --mem=10GB
+#SBATCH --array=1-6
+
+
+# MODULES #
+ml Python/3.6.4-foss-2018a
+# copy script from /projects/cegs/6vs16/Scripts/make_pseudogenome_fasta.py
+PSEUDOGENIZE=${WORK}Transcriptome/6vs16/Scripts/make_pseudogenome_fasta.py 
+
+# DATA #
+i=$PBS_ARRAY_INDEX
+mainDir=${WORK}Transcriptome/6vs16/Data/Genome/
+FASTA=${mainDir}TAIR10_genome.fasta
+VCFlst=${mainDir}vcf_for_pseudoGenome.txt
+VCF=${mainDir}$(sed -n ${i}p $VCFlst)
+OUT=${VCF/intersection/pseudoTAIR10}
+OUT=${OUT/.vcf/.fasta}
+
+# MAKE PSEUDO GENOMES #
+python $PSEUDOGENIZE -O $OUT $FASTA $VCF
+
+awk '/>[0-9]/{gsub(/>/,">Chr")}{print}' $OUT > ${OUT}.tmp
+mv ${OUT}.tmp $OUT
+
+
+
+
+
+
