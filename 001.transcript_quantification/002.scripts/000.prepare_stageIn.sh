86668cbcf37c3426082bed42a7bd66af28b3a337
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-11 17:31:04 +0200

    copyb TAIR Fasta file

--- 001.transcript_quantification/002.scripts/000.prepare_stageIn.sh
+++ 001.transcript_quantification/002.scripts/000.prepare_stageIn.sh
@@ -1,3 +1,6 @@
 
 # copy VCF to scratch
-cp -up /groups/nordborg/projects/the1001genomes/release/VCFs/1001genomes_snp-short-indel_only_ACGTN.vcf.gz
+cp -up /groups/nordborg/projects/the1001genomes/release/VCFs/1001genomes_snp-short-indel_only_ACGTN.vcf.gz /scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/001.data/
+
+# copy TAIR10 genome
+cp -up /groups/nordborg/user/pieter.clauw/Documents/Source/TAIR10/TAIR10_chr_all.fas /scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/001.data/
