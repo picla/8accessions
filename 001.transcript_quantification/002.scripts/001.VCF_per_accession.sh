c4a04e21a6d0d68b8e1686fd365c040cea9d3efa
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-14 11:49:00 +0200

    update VCF location

--- 001.transcript_quantification/002.scripts/001.VCF_per_accession.sh
+++ 001.transcript_quantification/002.scripts/001.VCF_per_accession.sh
@@ -11,7 +11,7 @@ ml vcftools/0.1.16-foss-2018b-perl-5.28.0
 # DATA #
 i=$SLURM_ARRAY_TASK_ID
 WORK=/scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/
-VCF=${WORK}001.data/1001genomes_snp-short-indel_only_ACGTN.vcf.gz
+VCF=${WORK}001.data/001.vcf/1001genomes_snp-short-indel_only_ACGTN.vcf.gz
 
 accessions=(6017 9728 9559 8242 9888 9433 9075)
 
