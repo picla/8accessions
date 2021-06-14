63b3ebc7aa689cb7dfcdcaf4e12abef0637296c5
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-14 13:29:06 +0200

    stage out results

---
+++ 001.transcript_quantification/002.scripts/006.stageOut.sh
@@ -0,0 +1,1 @@
+stage /scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/003.results/001.quantification_salmon/ /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/003.results/001.quantification_salmon/
