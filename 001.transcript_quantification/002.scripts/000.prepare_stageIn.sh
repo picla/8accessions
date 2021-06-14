365add7bb65441f7e4323eff1c7f77b0d735538b
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-14 13:28:55 +0200

    get folder structure from projects

--- 001.transcript_quantification/002.scripts/000.prepare_stageIn.sh
+++ 001.transcript_quantification/002.scripts/000.prepare_stageIn.sh
@@ -1,4 +1,10 @@
 
+# create folder structure
+PROJECTS=/groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/
+WORK=/scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/
+
+rsync -a --include '*/' --exclude '*' "$PROJECTS" "$WORK"
+
 # copy VCF to scratch
 cp -up /groups/nordborg/projects/the1001genomes/release/VCFs/1001genomes_snp-short-indel_only_ACGTN.vcf.gz /scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/001.data/001.vcf/
 
