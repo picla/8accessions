4c98665d323d2fc9a92818e5690a12972623a45d
picla <pieter.clauw@gmi.oeaw.ac.at>
2021-06-15 14:58:09 +0200

    create folder before staging out

--- 001.transcript_quantification/002.scripts/006.stageOut.sh
+++ 001.transcript_quantification/002.scripts/006.stageOut.sh
@@ -1,1 +1,3 @@
+
+mkdir -p /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/003.results/001.quantification_salmon/
 stage /scratch-cbe/users/pieter.clauw/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/003.results/001.quantification_salmon/ /groups/nordborg/projects/cold_adaptation_16Cvs6C/003.transcriptome/001.8accessions/001.transcript_quantification/003.results/001.quantification_salmon/
